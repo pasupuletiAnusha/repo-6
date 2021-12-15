df1397663919a604588dec66f3d4051d18e2faf5
pasupuletiAnusha <94434088+pasupuletiAnusha@users.noreply.github.com>
2021-12-15 12:25:39 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-       System.out.println(  "Hello ramya!");     
+       System.out.println(  "Hello anusha);     
     }
 }
